4c87a15732781ec9942afeacc86a738c5b85bcb8
A-Adegite <88291164+A-Adegite@users.noreply.github.com>
2025-02-07 09:27:50 +0100

    Add files via upload

---
+++ Stage-Zero/HackBio_Task0.py
@@ -0,0 +1,100 @@
+team_info = [
+    {
+        "Name" : "Adegite Adejuwon",
+        "Slack Username" : "a_adegite",
+        "E-mail" : "adegiteadejuwon@gmail.com",
+        "Hobby" : "Teaching",
+        "Country" : "Nigeria",
+        "Discipline" : "Cell Biology and Genetics",
+        "Preferred programming language" : "Python"
+     },
+        {
+        "Name" : ": Adewuyi Emmanuel",
+        "Slack Username" : "EmmanuelEA",
+        "E-mail" : "emmanueladewuyi69@gmail.com",
+        "Hobby" : "Photography",
+        "Country" : "Nigeria",
+        "Discipline" : "Biochemistry",
+        "Preferred programming language" : "Python"
+     },
+        {
+        "Name" : "Mgbafulu Chioma",
+        "Slack Username" : "Chioma",
+        "E-mail" : "chiomannamdi23@gmail.com",
+        "Hobby" : "listening to music",
+        "Country" : "Nigeria",
+        "Discipline" : " Pharmacy",
+        "Preferred programming language" : "Python"
+     },
+        {
+        "Name" : "Ajudua Sharon",
+        "Slack Username" : "SharonAjudua",
+        "E-mail" : "sharonajudua@gmail.com",
+        "Hobby" : "Trying out new recipes",
+        "Country" : "Nigeria",
+        "Discipline" : "Medicine",
+        "Preferred programming language" : "Python"
+     },
+        {
+        "Name" : "Maurice Godwin",
+        "Slack Username" : "Maurice Godwin",
+        "E-mail" : "mauricemary74@gmail.com",
+        "Hobby" : " Reading",
+        "Country" : "Nigeria",
+        "Discipline" : "Biotechnology",
+        "Preferred programming language" : "Python"
+     }
+]
+
+print("Team Members One Information:\n")
+print("Name:", team_info[0]["Name"])
+print("Slack Username:", team_info[0]["Slack Username"])
+print("E-mail:", team_info[0]["E-mail"])
+print("Hobby:", team_info[0]["Hobby"])
+print("Country:", team_info[0]["Country"])
+print("Discipline:", team_info[0]["Discipline"])
+print("Preferred Programming Language:", team_info[0]["Preferred programming language"])
+
+print() # This creates a space between team Members info
+
+print("Team Members Two Information:\n")
+print("Name:", team_info[1]["Name"])
+print("Slack Username:", team_info[1]["Slack Username"])
+print("E-mail:", team_info[1]["E-mail"])
+print("Hobby:", team_info[1]["Hobby"])
+print("Country:", team_info[1]["Country"])
+print("Discipline:", team_info[1]["Discipline"])
+print("Preferred Programming Language:", team_info[1]["Preferred programming language"])
+
+print() # This creates a space between team Members info
+
+print("Team Members Three Information:\n")
+print("Name:", team_info[2]["Name"])
+print("Slack Username:", team_info[2]["Slack Username"])
+print("E-mail:", team_info[2]["E-mail"])
+print("Hobby:", team_info[2]["Hobby"])
+print("Country:", team_info[2]["Country"])
+print("Discipline:", team_info[2]["Discipline"])
+print("Preferred Programming Language:", team_info[2]["Preferred programming language"])
+
+print() # This creates a space between team Members info
+
+print("Team Members Four Information:\n")
+print("Name:", team_info[3]["Name"])
+print("Slack Username:", team_info[3]["Slack Username"])
+print("E-mail:", team_info[3]["E-mail"])
+print("Hobby:", team_info[3]["Hobby"])
+print("Country:", team_info[3]["Country"])
+print("Discipline:", team_info[3]["Discipline"])
+print("Preferred Programming Language:", team_info[3]["Preferred programming language"])
+
+print() # This creates a space between team Members info
+
+print("Team Members Five Information:\n")
+print("Name:", team_info[4]["Name"])
+print("Slack Username:", team_info[4]["Slack Username"])
+print("E-mail:", team_info[4]["E-mail"])
+print("Hobby:", team_info[4]["Hobby"])
+print("Country:", team_info[4]["Country"])
+print("Discipline:", team_info[4]["Discipline"])
+print("Preferred Programming Language:", team_info[4]["Preferred programming language"])
